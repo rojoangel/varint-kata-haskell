9170a1d57a998a2f1f1c241966d33838dd947eb6
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-09-01 23:30:04 +0200

    add failing test for fromVarint

--- Varint.Tests.hs
+++ Varint.Tests.hs
@@ -1,8 +1,11 @@
 module Varint.Test where
-import Varint (varint)
+import Varint (toVarint, fromVarint)
 import Test.Hspec
 
 main = hspec $ do
-  describe "varint" $ do
-    it "should work on the examples" $ do
-      varint 300 `shouldBe` "1010 1100 0000 0010"
+  describe "toVarint" $ do
+    it "should work on the toVarint examples" $ do
+      toVarint 300 `shouldBe` "1010 1100 0000 0010"
+  describe "fromVarint" $ do
+    it "should work on the fromVarint examples" $ do
+      fromVarint "1010 1100 0000 0010" `shouldBe` 300
