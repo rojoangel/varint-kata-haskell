bf33aa02b63133cc6a7ffcc41322893a4708bd3a
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-09-02 00:32:20 +0200

    add round tests

--- Varint.Tests.hs
+++ Varint.Tests.hs
@@ -9,3 +9,7 @@ main = hspec $ do
   describe "fromVarint" $ do
     it "should work on the fromVarint examples" $ do
       fromVarint "1010 1100 0000 0010" `shouldBe` 300
+  describe "round test" $ do
+    it "toVarint and fromVarint should work together" $ do
+      fromVarint(toVarint 300) `shouldBe` 300
+      map (fromVarint . toVarint) [1..300000] `shouldBe` [1..300000]
